e654b5b3124c26928f7ff698b3a98b3a59f69e1f
Andreas <160026130+AndreasPJensen@users.noreply.github.com>
2024-05-15 21:27:13 +0200

    Update 03_evaluate_classifier.py

--- 03_evaluate_classifier.py
+++ 03_evaluate_classifier.py
@@ -1,4 +1,4 @@
-import pickle #for loading your trained classifier
+import pickle # loading your trained classifier
 
 from extract_features import extract_features #our feature extraction
 
@@ -9,21 +9,16 @@ def classify(img):
     Insert only name of image, must be of type ".png"    
     """
         
-     #Extract features (the same ones that you used for training)
+     # Extract features (the same ones that we used for training)
     x = extract_features(img)
-         
-     
-     #Load the trained classifier
+    
+    #Load the trained classifier
     classifier = pickle.load(open('groupNJ_classifier.sav', 'rb'))
     
-    
-     #Use it on this example to predict the label AND posterior probability
+    # Use it on this example to predict the label AND posterior probability
     pred_label = classifier.predict(x)
     pred_prob = classifier.predict_proba(x)
      
-     
-     #print('predicted label is ', pred_label)
-     #print('predicted probability is ', pred_prob)
     return pred_label, pred_prob
  
     
